5f7839e8935009187ef6c481e090c9cf6a156f8a
Hayden Young <hayden@itshayden.dev>
2020-07-19 18:30:51 +0100

    Create shell.nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+with import <nixpkgs> {};
+
+stdenv.mkDerivation {
+  name = "node";
+  buildInputs = [
+    nodejs
+  ];
+  shellHook = ''
+    export PATH="$PWD/node_modules/.bin/:$PATH"
+
+    alias run='npm run'
+  '';
+}
